44103c52b00bff91a017b51419101e8461c5857a
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-19 20:34:50 +0530

    Update converter.py

--- Music/services/converter/converter.py
+++ Music/services/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from SheebaMusic.helpers.errors import FFmpegReturnCodeError
+from Music.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
